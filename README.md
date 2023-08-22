78bd3ce6fa5d201c48a768e2148266df5eee3467
christerner <christianuwayo250@gmail.com>
2023-08-22 15:33:24 -0700

    Updated

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 This is a simple application you can use to search for some information related to any country around the globe.
 I have been enjoying working with exernal free api and retrieve some information, which was fun.
 I hope to learn more beyond this and gain web development skills.
-countries-app
--- script.js
+++ script.js
@@ -1,103 +1,51 @@
-let containerAll = document.querySelector(".countries-container");
-let viewCountry = document.querySelector(".view-country");
-let loading = document.querySelector(".loading");
-let byRegion = document.querySelector("#region")
-let search = document.querySelector(".search-box");
-let displayByRegion = document.querySelector(".view-region");
-let errorMsg = document.querySelector(".error-message");
+const countriesContainer = document.querySelector('.countries-container')
+const filterByRegion = document.querySelector('.filter-by-region')
+const searchInput = document.querySelector('.search-container input')
+const themeChanger = document.querySelector('.theme-changer')
 
-const apiURL = "https://restcountries.com/v3.1/";
+let allCountriesData
 
-// fetch all countries
-async function fetchCountries() {
-    let response = await fetch(apiURL + "all");
-    let data = await response.json();
+fetch('https://restcountries.com/v3.1/all')
+  .then((res) => res.json())
+  .then((data) => {
+    renderCountries(data)
+    allCountriesData = data
+  })
 
-    let display = "";
-
-    data.forEach(country => {
-
-        display += `<div class="country-card">
-        <img src=${country.flags.svg} alt="${country.name.common}">
-        <h4 class="">${country.name.common}</h4>
-        <p class="region">Region: ${country.region}</p>
-        <p class="capital">Capital: ${country.capital}</p>
-        <p class="population">Population: ${country.population}</p>
-    </div>`
-    })
-    containerAll.innerHTML = display;
-}
-
-fetchCountries();
-
-// Filter by region
-async function filterByRegion(region) {
-    let response = await fetch(apiURL + `region/${region}`);
-    let data = await response.json(); 
-
-    let display = "";
-
-    data.forEach(country => {
-
-        display += `<div class="country-card">
-        <img src=${country.flags.svg} alt="${country.name.common}">
-        <h4 class="">${country.name.common}</h4>
-        <p class="region">Region: ${country.region}</p>
-        <p class="capital">Capital: ${country.capital}</p>
-        <p class="population">Population: ${country.population}</p>
-    </div>`
-    })
-
-    containerAll.innerHTML = display;
-}
-
-byRegion.addEventListener("click", () => {
-    let query = byRegion.value;
-
-    filterByRegion(query)
+filterByRegion.addEventListener('change', (e) => {
+  fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+    .then((res) => res.json())
+    .then(renderCountries)
 })
 
-// search for a country
-async function searchCountry(query) {  
-    if(searchBtn.value == 0) {
-        errorMsg.innerHTML = "Please enter country name"
-     } 
-
-    let response = await fetch(apiURL + `name/${query}`);
-    let data = await response.json();
-    if(data.length < 1)   errorMsg.innerHTML = "No data"
-    console.log(data)     
-    errorMsg.innerHTML = "" 
-    searchBtn.value = "";    
-
-    let display = "";
-     
-        display += `<div class="country-card">
-            <img src=${data[0].flags.svg} alt="${data[0].name.common}">
-            <h4 class="">${data[0].name.common}</h4>
-            <p class="region">Region: ${data[0].region}</p>
-            <p class="capital">Capital: ${data[0].capital}</p>
-            <p class="population">Population: ${data[0].population}</p>  
-            <a href="index.html"><button class="back-btn">Back</button></a>          
-        </div>`
-    
-    containerAll.innerHTML = display;
-    
-
-
-
-    // console.log(data[0].subregion)
-    // console.log(data[0].capital)
-    // console.log(data[0].population)
+function renderCountries(data) {
+  countriesContainer.innerHTML = ''
+  data.forEach((country) => {
+    const countryCard = document.createElement('a')
+    countryCard.classList.add('country-card')
+    countryCard.href = `/country.html?name=${country.name.common}`
+    countryCard.innerHTML = `
+          <img src="${country.flags.svg}" alt="${country.name.common} flag" />
+          <div class="card-text">
+              <h3 class="card-title">${country.name.common}</h3>
+              <p><b>Population: </b>${country.population.toLocaleString(
+                'en-IN'
+              )}</p>
+              <p><b>Region: </b>${country.region}</p>
+              <p><b>Capital: </b>${country.capital?.[0]}</p>
+          </div>
+  `
+    countriesContainer.append(countryCard)
+  })
 }
-let searchBtn = document.querySelector("button");
-searchBtn.addEventListener("click", () => {
-    
-    errorMsg.innerHTML = ""
-    let value = search.value;
-    //console.log(value)
-    searchCountry(value);
-   
-    
-    
+
+
+searchInput.addEventListener('input',  (e) => {
+  const filteredCountries = allCountriesData.filter((country) => country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+  renderCountries(filteredCountries)
 })
+
+themeChanger.addEventListener('click', () => {
+  document.body.classList.toggle('dark')
+})
+
